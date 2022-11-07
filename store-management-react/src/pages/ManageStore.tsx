8f0170230398c1071e7cfacf8e02fb27b31dc21f
nicoleburgess1 <nburgess1113@gmail.com>
2022-11-07 18:02:24 -0500

    started managestore

--- store-management-react/src/pages/ManageStore.tsx
+++ store-management-react/src/pages/ManageStore.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Button from '@mui/material/Button';
 
 function ManageStore() {
+
+
     return (
         <div className={"page"}>
             <h1>Manage Store #</h1>
@@ -10,12 +12,8 @@ function ManageStore() {
                 <Button variant="contained">Fill To Max</Button>
                 <Button variant="contained">Generate Report</Button>
             </div>
-            <select name="items" id="items">
-                <option value="volvo">Volvo</option>
-                <option value="saab">Saab</option>
-                <option value="opel">Opel</option>
-                <option value="audi">Audi</option>
-            </select>
+
+
 
             <Button variant="contained">Process Shipment</Button>
         </div>
