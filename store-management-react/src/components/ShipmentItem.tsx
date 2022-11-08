be1aab4fd0047bb2c17294fd700e720919b3817b
MargaretEarnest <34314628+MargaretEarnest@users.noreply.github.com>
2022-11-07 19:16:00 -0500

    Added ShipmentItem styling

--- store-management-react/src/components/ShipmentItem.tsx
+++ store-management-react/src/components/ShipmentItem.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
-import AddCircleTwoToneIcon from "@mui/icons-material/AddCircleTwoTone";
 import IconButton from "@mui/material/IconButton";
 
 const ShipmentItem = (props:{label:string}) => (
     <div className={"shipmentItem"}>
-        <div>
+        <div style={{color:"white"}}>
             {props.label}
         </div>
-        <IconButton style={{color:"white"}}><AddCircleTwoToneIcon></AddCircleTwoToneIcon></IconButton>
+        <IconButton style={{color:"white"}}><CancelOutlinedIcon/></IconButton>
     </div>
 );
 
