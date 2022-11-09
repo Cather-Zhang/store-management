93ad0ad3230e70462a520dfd85fad9a638a29457
Bao Huynh <bdhuynh@wpi.edu>
2022-11-09 00:07:50 +0000

    add overstock table

---
+++ store-management-react/src/components/tables/ItemInOverstockTable.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {ItemLocation} from "../../types/ItemLocation";
+import BaseTable from "./BaseTable";
+import {Stock} from "../../types/Stock";
+
+export default function ItemInStoreTable(props: { stockWithLocation: {location: ItemLocation, stock: Stock}[] }) {
+    return <BaseTable className={"itemInStoreTable"} headers={["Name", "Aisle", "Shelf", "Price ($)", "Quantity"]}
+                      data={props.stockWithLocation.map((swl: {location: ItemLocation, stock: Stock }, i) => {
+                          let item = swl.stock.item;
+                          return {
+                              id: i,
+                              columns: [item.name, swl.location.aisle, swl.location.shelf, item.price, swl.stock.quantity]
+                          };
+                      })}/>
+}
