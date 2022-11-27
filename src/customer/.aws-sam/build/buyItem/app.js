5110739a06dc3ae6e4efab65c90edbd83627adaa
Bao Huynh <bdhuynh@wpi.edu>
2022-11-27 18:40:05 +0000

    Fix APIs

--- src/customer/.aws-sam/build/buyItem/app.js
+++ src/customer/.aws-sam/build/buyItem/app.js
@@ -11,6 +11,32 @@ var pool = mysql.createPool({
     database: config.database
 });
 
+class Item {
+    constructor(sku, name, description, price, max) {
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.max = max;
+    }
+}
+
+class Shopping {
+    constructor(sku, location, remain) {
+        this.sku = sku;
+        this.location = location;
+        this.remain = remain;
+    }
+}
+
+class Location {
+    constructor(aisle, shelf) {
+        this.aisle = aisle;
+        this.shelf = shelf;
+    }
+}
+
+
 
 function query(conx, sql, params) {
     return new Promise((resolve, reject) => {
@@ -56,72 +82,73 @@ exports.lambdaHandler = async (event, context, callback) => {
     let info = JSON.parse(actual_event);
     console.log("info:" + JSON.stringify(info)); 
     
-    function hash(string) {
-        //set variable hash as 0
-        var hash = 0;
-        // if the length of the string is 0, return 0
-        if (string.length == 0) return hash;
-        for (let i = 0 ;i<string.length ; i++)
-        {
-        let ch = string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + ch;
-        hash = hash & hash;
+    // check availability of an item given storeID, sku, aisle and shelf
+    let checkItemAvailability = (idStore, sku, aisle, shelf, quantity) => {
+        let item_sku = JSON.Parse(sku);
+        let item_aisle = parseInt(aisle);
+        let item_shelf = parseInt(shelf);
+        let item_quantity = parseInt(quantity);
+        if (isNaN(item_sku) || isNaN(item_aisle) || isNaN(item_shelf) ||  isNaN(item_quantity)) {
+            return new Promise((reject) => { return reject("invalid item's purchase input")});
         }
-        return hash;
+        if(item_quantity <=0){
+            return new Promise((reject) => { return reject("invalid quantity")});
+        }
+        
+        return new Promise((resolve, reject) => {
+            pool.query("SELECT * FROM Stocks WHERE idStores=? AND sku=? AND aisle=? AND shelf=? AND quantity>=? AND onShelf=true", [idStore, item_sku, item_aisle, item_shelf, item_quantity], (error, rows) => {
+                if (error) { return reject(error); }
+                let shopping = [];
+                if (rows.length > 0) {
+                    for (let r of rows) {
+                        let remaining = r.quantity - item_quantity;
+                        let cart = new Shopping(r.sku, new Location(r.aisle, r.shelf), remaining);
+                        shopping.push(cart);
+                    }
+                    return resolve(shopping);
+                } else {
+                    return resolve(false);
+                }
+            });
+        });
     }
     
-    let password_hashed = hash(info.password)
-    
-    // check corporate, if yes return true, else return false
-    let isCorporate = (username, password) => {
+    // update availability of an item given storeID, sku, aisle and shelf
+    let updateAvailability = (idStore, sku, location, quantity) => {
         return new Promise((resolve, reject) => {
-            pool.query("SELECT * FROM Users WHERE username=? AND password=?", [username, password], (error, rows) => {
-                if (error) { return reject(error); }
-                if ((rows) && (rows.length == 1)) {
+            pool.query("UPDATE Stocks SET quantity=? WHERE idStores=? AND sku=? AND aisle=? AND shelf=? AND onShelf=true", [quantity, idStore, sku, location.aisle, location.shelf], (error, rows) => {
+                if (error) { return reject(error);} 
+                else {
                     return resolve(true);
-                } else {
-                    return resolve(false);
                 }
             });
         });
     }
-    
-    // check manager, if is return store id, if not return false
-    let isManager = (username, password) => {
-        return new Promise((resolve, reject) => {
-            pool.query("SELECT * FROM Stores WHERE manager=? AND password=?", [username, password], (error, rows) => {
-                if (error) { return reject(error); }
-                if ((rows) && (rows.length == 1)) {
-                    return resolve(rows[0].idStores);
-                } else {
-                    return resolve(false);
-                }
-            });
-        });
-    }
-    
     
     try {
+        const idStore = parseInt(info.storeId);
+        const checkType = JSON.Parse(info.type);
+        if(!(checkType == "buy")){
+            response.status = 400;
+            response.error = "can not fetch purchase";
+        }
         
-        const isCorporateReturn = await isCorporate(info.username, password_hashed);
-        // const ret = await axios(url);
-        if (isCorporateReturn == true) {
-            response.status = 200;
-            response.role = "corporate";
-
+        const items = await checkItemAvailability(idStore, info.sku, info.aisle, info.shelf, info.quantity);
+        if (!(items == false)) {
+            for (let item of items) {
+                let update = await updateAvailability(idStore, item.sku, item.location, item.remain);
+                if(update){
+                    response.status = 200;
+                }else{
+                    response.status = 400;
+                    response.error = "cannot update availability";
+                }
+            }
+            
         }
         else {
-            const isManagerReturn = await isManager(info.username, password_hashed);
-            if (isManagerReturn == false) {
-                response.status = 400;
-                response.error = "invalid username and password"
-                
-            }
-            else {
-                response.status = 200;
-                response.role = "manager";
-                response.storeId = isManagerReturn;
-            }
+            response.status = 400;
+            response.error = "no item available";
         }
 
     } catch (error) {
