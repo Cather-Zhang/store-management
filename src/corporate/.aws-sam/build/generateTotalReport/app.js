defc5a28607a1ffcce061e29f4d7ca1c9d57a1db
Bao Huynh <bdhuynh@wpi.edu>
2022-12-15 04:59:04 +0000

    Fix generate total report - corporate

--- src/corporate/.aws-sam/build/generateTotalReport/app.js
+++ src/corporate/.aws-sam/build/generateTotalReport/app.js
@@ -11,22 +11,44 @@ var pool = mysql.createPool({
     database: config.database
 });
 
-class Item {
-    constructor(sku, name, description, price, max) {
-        this.sku = sku;
+
+class Store {
+    constructor(id, name, latitude, longitude, managername) {
+        this.idStores = id;
         this.name = name;
-        this.description = description;
-        this.price = price;
-        this.max = max;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.manager = managername;
     }
 }
 
-class Stock {
-    constructor(item,quantity) {
-        this.item = item;
-        this.quantity = quantity;
+
+class StoreInventory {
+    constructor(id, name, latitude, longitude, totalValue) {
+        this.idStores = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.totalValue = totalValue;
     }
 }
+
+// class Item {
+//     constructor(sku, name, description, price, max) {
+//         this.sku = sku;
+//         this.name = name;
+//         this.description = description;
+//         this.price = price;
+//         this.max = max;
+//     }
+// }
+
+// class Stock {
+//     constructor(item,quantity) {
+//         this.item = item;
+//         this.quantity = quantity;
+//     }
+// }
 
 
 function query(conx, sql, params) {
@@ -75,25 +97,19 @@ exports.lambdaHandler = async (event, context, callback) => {
     }; // response
     
     
-    //list all items
-    let listAllItems = () => {
+    //list all stores
+    let listAllStores = () => {
         return new Promise((resolve, reject) => {
-                pool.query("SELECT * FROM Items", [], (error, rows) => {
+                pool.query("SELECT * FROM Stores", [], (error, rows) => {
                     if (error) { return reject(error); }
                     if (rows) {
-                        let items = [];
+                        let stores = [];
                         for (let r of rows) {
-                            let sku = r.sku;
-                            let name = r.name;
-                            let price = r.price;
-                            let max = r.max;
-                            let description = r.description;
-                            let newItem = new Item(sku, name, description, price, max);
-
-                            items.push(newItem);
+                            let newStore = new Store(r.idStores, r.name, r.latitude, r.longitude, r.manager);
+                            stores.push(newStore);
                         }
 
-                        return resolve(items);
+                        return resolve(stores);
                     } else {
                         return resolve(false);
                     }
@@ -103,14 +119,15 @@ exports.lambdaHandler = async (event, context, callback) => {
     
   
         //find if items are already stored on shelf
-    let getStocks = (sku) => {
+    let getTotalStocksValue = (idStores) => {
         return new Promise((resolve, reject) => {
-            pool.query("SELECT * FROM Stocks WHERE sku=?", [sku], (error, rows) => {
+            pool.query("SELECT SUM(S.quantity*I.price) as total FROM Stocks S " + 
+                       "JOIN Items I on S.sku = I.sku where S.idStores = ?", [idStores], (error, rows) => {
                 if (error) { return reject(error); }
                 if (rows.length > 0) {
                     let sum = 0;
                     for (let r of rows) {
-                        sum = sum + r.quantity;
+                        sum = sum + r.total;
                     }
                     return resolve(sum);
                 } else {
@@ -121,32 +138,30 @@ exports.lambdaHandler = async (event, context, callback) => {
     }
     
     
-    let totalValue = (stocks) => {
-        let total = 0;
-        for (let stock of stocks) {
-            let price = stock.item.price;
-            let quantity = stock.quantity;
-            total = total + price * quantity;
+    // let totalValue = (stocks) => {
+    //     let total = 0;
+    //     for (let stock of stocks) {
+    //         let price = stock.item.price;
+    //         let quantity = stock.quantity;
+    //         total = total + price * quantity;
            
-        }
-        return total;
-    }
+    //     }
+    //     return total;
+    // }
     
     try {
 
-        const items = await listAllItems();
-        if (!(items == false)) {
-            let stocks = [];
+        const stores = await listAllStores();
+        if (!(stores == false)) {
+            let allStores = [];
             
-            for (let item of items) {
-                let inventoriesOnShelf = await getStocks(item.sku);
-                let s = new Stock(item, inventoriesOnShelf);
-                stocks.push(s);
-                
+            for (let store of stores) {
+                let totalValue = await getTotalStocksValue(store.idStores);
+                let storeInv = new StoreInventory(store.idStores, store.name, store.latitude, store.longitude, totalValue);
+                allStores.push(storeInv);
             }
             response.status = 200;
-            response.stocks = JSON.parse(JSON.stringify(stocks));
-            response.totalValue = totalValue(stocks);
+            response.stocks = JSON.parse(JSON.stringify(allStores));
         }
         else {
             response.status = 400;
